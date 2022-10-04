5391ec69d66c0b75c7582a64a8de15b899f335c3
davidcoats <david@getsafetycone.com>
2022-10-04 16:01:53 -0700

    Initial commit.

---
+++ source/R5T.Z0012/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.Z0012
+{
+	/// <summary>
+	/// Common directory names.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.Z0012/Code/Instances.cs
@@ -0,0 +1,10 @@
+using System;
+
+
+namespace R5T.Z0012
+{
+    public static class Instances
+    {
+		
+    }
+}
