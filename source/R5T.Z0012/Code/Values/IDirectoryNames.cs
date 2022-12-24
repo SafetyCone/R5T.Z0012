46de3f805f760a1283c0099834fdf71605323923
davidcoats <david@getsafetycone.com>
2022-12-23 23:27:42 -0800

    Interim changes.

--- source/R5T.Z0012/Code/Values/IDirectoryNames.cs
+++ source/R5T.Z0012/Code/Values/IDirectoryNames.cs
@@ -10,10 +10,13 @@ namespace R5T.Z0012
 	{
 #pragma warning disable IDE1006 // Naming Styles
         public string bin => "bin";
+		public string obj => "obj";
 #pragma warning restore IDE1006 // Naming Styles
 
 		public string Binaries => this.bin;
 		public string Debug => "Debug";
+		public string Objects => this.obj;
+		public string Publish => "publish";
 		public string Release => "Release";
     }
 }
