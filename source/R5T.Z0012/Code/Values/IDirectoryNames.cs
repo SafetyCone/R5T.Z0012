8010fc27edbf410d8a95cdd31446f7e46b798fe3
davidcoats <david@getsafetycone.com>
2024-05-30 22:26:01 -0700

    Interim changes.

--- source/R5T.Z0012/Code/Values/IDirectoryNames.cs
+++ source/R5T.Z0012/Code/Values/IDirectoryNames.cs
@@ -9,13 +9,22 @@ namespace R5T.Z0012
 	public partial interface IDirectoryNames : IValuesMarker
 	{
 #pragma warning disable IDE1006 // Naming Styles
+
+		/// <summary>
+		/// <para><value>bin</value></para>
+		/// </summary>
         public string bin => "bin";
 		public string obj => "obj";
+
 #pragma warning restore IDE1006 // Naming Styles
 
 		public string Binaries => this.bin;
 		public string Debug => "Debug";
 		public string Objects => this.obj;
+
+		/// <summary>
+		/// <para><value>publish</value></para>
+		/// </summary>
 		public string Publish => "publish";
 		public string Release => "Release";
     }
